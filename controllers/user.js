f435febdc155214a105fc42f9daabf24ba3de317
jimynu <kim@bylr.ch>
2017-11-29 15:45:10 +0100

    corrections, refactoring

--- controllers/user.js
+++ controllers/user.js
@@ -28,11 +28,14 @@ router.get( '/:id', (req, res, next) => {
 
 
 router.put( '/:id', (req, res, next) => {
-  // mask data (only pass on qualifying values)
+  // mask data
+  const { level, newbie, name } = req.body;
   const updateData = {};
-  if ( req.body.level ) updateData.level = req.body.level;
-  if ( req.body.newbie === false || req.body.newbie === true ) updateData.newbie = req.body.newbie;
-  if ( req.body.name ) updateData.name = req.body.name;
+
+  // only pass on qualifying values
+  if ( [10, 20, 30].indexOf(updateData.level) !== -1 ) updateData.level = level;
+  if ( typeof updateData.newbie === "boolean" ) updateData.newbie = newbie;
+  if ( updateData.name ) updateData.name = name;
 
   req.user.update( updateData, (error, result) => {
     if ( error ) return next(error);
@@ -52,7 +55,6 @@ router.get( '/:id/lexemes', (req, res, next) => {
 
 router.put( '/:id/lexemes', (req, res, next) => {
 
-  // move to models.js?
   User.findById( req.user._id )
     .then( ({ lexemes: oldLexemes }) => {
 
@@ -63,7 +65,7 @@ router.put( '/:id/lexemes', (req, res, next) => {
       // add
       const lexemesToAdd = req.body.add
         .filter( lexemeId => oldLexemes.map( ({lexeme: id}) => String(id) ).indexOf(lexemeId) === -1 ) // filter out if already in lexeme list
-        .map( lexeme => { return {lexeme} } );
+        .map( lexeme => ({lexeme}) );
 
       // persist in DB, deliver response
       req.user.update( { lexemes: [...lexemesFiltered, ...lexemesToAdd] }, (error, result) => {
@@ -90,7 +92,7 @@ router.get( '/:id/lexemes/:quantity', (req, res, next) => {
 router.put( '/:id/lexemes/:lexemeId/:success', (req, res, next) => {
   User.findById( req.user._id )
     .then( user => {
-      const lexeme = user.lexemes.filter( ({ lexeme: id }) => String(id) === req.params.lexemeId )[0];
+      const lexeme = user.lexemes.find( ({ lexeme: id }) => String(id) === req.params.lexemeId );
 
       if(!lexeme) {
         const error = new Error( 'Lexeme not found' );
@@ -98,40 +100,28 @@ router.put( '/:id/lexemes/:lexemeId/:success', (req, res, next) => {
         return next(error)
       }
 
-      const success = req.params.success
-
+      const success = req.params.success;
       if (success.search(/^(correct|wrong)$/) === -1 ) {
         const error = new Error('Bad request');
         error.status = 400;
         return next(error);
       }
 
-      if ( success === 'correct' ) lexeme.times_right++;
-      if ( success === 'wrong' ) lexeme.times_wrong++;
+      if (success === 'correct') {
+        lexeme.correctAnswers++;
+        lexeme.progress++;
+      } else if (success === 'wrong' ) {
+        lexeme.wrongAnswers++;
+        lexeme.progress /= 2;
+      }
 
       user.updateProgress(lexeme, (error, result) => {
         if ( error ) return next(error);
         res.json(result);
       });
-
-
     })
     .catch( error => next(error) );
 });
 
 
 module.exports = router;
-
-
-
-router.delete( '/:id', (req, res, next) => {
-  if ( req.params.id.search(/^(up|down)$/) === -1 ) {
-    var err = new Error('only up or down, mate');
-    err.status = 404;
-    next(err);
-  } else {
-    next();
-  }
-}, (req, res) => {
-  res.json({ hui: req.params.id });
-});
